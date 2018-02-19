6e68088430f7424e24943fe585ed20301b1b703d
Nick Featherstone <feathern@colorado.edu>
2018-02-19 13:11:04 -0700

    Added python messages and imessages.py Fixed waitall issue in ex1

--- Advanced_P2P/exercises/Python/ex1.py
+++ Advanced_P2P/exercises/Python/ex1.py
@@ -130,9 +130,10 @@ if (rank == 0):
             reqs.append(rreq)
             offset = offset+counts[i]
 
-    # Call the wait method of each request
-    for i in range(len(reqs)):
-        reqs[i].Wait()
+    # Call the Waitall method of the last Request generated (if one exists)
+    # We pass a list of all requests collected
+    if (found):
+        rreq.Waitall(reqs)
 
     # Finally, have rank 0 print out the results
     for i in range(ns):
---
+++ Advanced_P2P/mpi_imessages.py
@@ -0,0 +1,81 @@
+from mpi4py import MPI
+import sys
+import numpy
+import random
+import os
+
+###################################################
+# Fill in the body of this custom allreduce function
+# (use a series of calls to Send and Recv)
+
+# There are many more clever ways to do this.  Here, we take a
+# straightforward approach and use logic similar to our earlier
+# deadlock exercise (exercise 3)
+
+
+
+###########################
+# Initialize Communication
+cw = MPI.COMM_WORLD
+rank = cw.rank
+size = cw.size
+proc = MPI.Get_processor_name()
+
+
+if (rank == 0):
+    print(str(size)+' MPI processes are now active')
+
+
+# In this example, ranks 0 and num_proc-1 send each other a single integer
+# We use blocking Sends and Recvs.  To avoid deadlock, rank size-1 sends first, 
+# then receives.  Rank 0, on the other hand, receives first, and then sends
+
+stoken = 3*rank+1  # The number ranks 0 and size-1 send
+
+
+
+if (rank == (size-1)):
+    mydest = 0     # The destination rank
+    mytag = rank   # A unique tag for this message( dest should expect the same tag)
+    nvals = 1    # The number of values we are sending
+
+    # We create a single-element numpy array to hold our value
+    svals = numpy.zeros(nvals,dtype='i')
+    svals[0] = stoken
+
+    # ISend and Irecv are METHODS of the MPI Communicator (which we call cw)
+    # They RETURN a Request object whose status we can query to check if the 
+    # exchange has completed
+    sreq = cw.Isend(svals,dest=mydest,tag=mytag )
+
+    # Next, num_proc -1 receives a message from rank 0
+    mysource = 0
+    yourtag = 0
+    nvals =1
+
+    rvals = numpy.zeros(nvals,dtype='i')
+    rreq = cw.Irecv(rvals,source=mysource,tag=yourtag)
+
+    rreq.Waitall([sreq,rreq])
+
+if (rank == 0):
+    mysource = size-1
+    yourtag = size-1
+    nvals = 1
+    svals = numpy.zeros(nvals,dtype='i')
+    rvals = numpy.zeros(nvals,dtype='i')
+
+    rreq = cw.Irecv(rvals,source=mysource,tag=yourtag) # Receive from rank size-1
+
+    mydest = size-1
+    mytag = rank
+    sreq = cw.Isend(svals,dest=mydest,tag=mytag) # Send to rank size-1
+
+    sreq.Waitall([sreq,rreq])
+
+if ((rank == 0) or  (rank == size-1)):
+    print('Rank '+str(rank)+' has received the value '+str(rvals[0]) +' from rank '+str(mysource)+'.')
+
+
+
+MPI.Finalize()
