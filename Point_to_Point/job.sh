2df72a1782870ffa3283388f71983729006b1a22
Nick Featherstone <feathern@colorado.edu>
2018-02-20 09:33:18 -0700

    updated reservation in job script

--- Point_to_Point/job.sh
+++ Point_to_Point/job.sh
@@ -6,12 +6,19 @@
 #SBATCH --job-name=scaling_tutorial                    # Job submission name
 #SBATCH --output=scaling.%j.out               # Output file name with Job ID
 #SBATCH -A training
+#SBATCH --reservation=training
 
 # purge all existing modules
 module purge
 
 # load the compiler and mpi
 module load intel/psxe-2018u1
+
+# or load python
+# module purge
+# module load python/pPython3
+
+
 
 # run the code (C/Fortran)
 srun --mpi=pmi2 -n 2 ./mpi_messages.out
