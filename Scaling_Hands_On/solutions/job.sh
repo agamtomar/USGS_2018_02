65920b34851665c7581a260b2d01d8a8c8edbb75
Nick Featherstone <feathern@colorado.edu>
2018-02-20 09:31:34 -0700

    jobscript uses correct reservation name

--- Scaling_Hands_On/solutions/job.sh
+++ Scaling_Hands_On/solutions/job.sh
@@ -6,12 +6,14 @@
 #SBATCH --job-name=scaling_tutorial                    # Job submission name
 #SBATCH --output=scaling.%j.out               # Output file name with Job ID
 #SBATCH -A training
+#SBATCH --reservation=training
 
 # purge all existing modules
 module purge
 
 # load the compiler and mpi
 module load intel/psxe-2018u1
+
 
 #
 bash scaling_script.sh
