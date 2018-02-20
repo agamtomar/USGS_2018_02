c4b74c2903634b8b232a5191bdb99088cd4a5d69
Thomas Hauser <thauser@igskahcmgslih04.cr.usgs.gov>
2018-02-20 14:57:08 -0700

    example 5 fixed

--- Point_to_Point/exercises/R/solutions/ex5.R
+++ Point_to_Point/exercises/R/solutions/ex5.R
@@ -1,31 +1,45 @@
 #/////////////////////////////////////////
-#   Token-Passing Deadlock Exercise
-#  
-#   Calling sequence:
-#       mpiexec -np X python ex3.py -N Y
-#       where Y indicates the number of integers sent during each pass
-#       
-#   Results:
-#       When the program completes successfully, each process will possess a buffer containing
-#       num_proc*Y values, with the value of each mpi rank occurring Y times.'
-#
-#   Example 1:
-#           mpiexec -np 3 Rscript ex3.R -N 1 
-#               gives
-#           my_rank = 0; buffer =  0 2 1
-#           my_rank = 1; buffer =  1 0 2
-#           my_rank = 2; buffer =  2 1 0
-#       
-#   Example 2:
-#           mpiexec -np 4 Rscript ex3.R -N 2
-#               gives
-#           my_rank = 0; buffer =  0 0 3 3 2 2 1 1
-#           my_rank = 1; buffer =  1 1 0 0 3 3 2 2
-#           my_rank = 2; buffer =  2 2 1 1 0 0 3 3
-#           my_rank = 3; buffer =  3 3 2 2 1 1 0 0
-#
-#   NOTE:  depending on your bufferflush settings, the lines above may appear out of order.
+# 
 suppressMessages(library(pbdMPI, quietly = TRUE))
+
+myAllReduce <- function(myrank, size, valin, rop)
+{
+
+    left = rank - 1
+    right = rank + 1
+    if ( right > (size-1)) {
+        right  = 0
+    }
+    if ( left < 0 ) {
+        left = size-1
+    }
+    parity = rank %% 2
+
+    valout = valin
+    sval = valout
+    rval = 0
+    for (i in 1:(size-1)) {
+        mtag = 1
+        if (parity == 0) {
+            send(sval, rank.dest=right, tag=mtag)
+            recv(rval, rank.source=left, tag=mtag)
+        }
+        else {
+            recv(rval, rank.source=left, tag=mtag)
+            send(sval, rank.dest=right, tag=mtag)
+        }
+        if (rop == 1) {
+            valout = valout+rval
+            sval = rval
+        }
+        if (rop == 2) {
+            valout = valout*rval
+            sval = rval
+        }
+    }
+    return(valout)
+}
+
 
 # initialize MPI
 init()
@@ -36,56 +50,16 @@ if (rank == 0) {
     print(sprintf("%d MPI processes are active", size))
     args = commandArgs(TRUE)
     n = as.integer(args[1])
-    print(sprintf("n is: %d", n))
+    print(sprintf("Operation is: %d", n))
     nb = bcast(n)
 } else {
     n = 0
     nb = bcast(n)
 }
 
-## initialize the data
-mybuffer = integer(nb*size)
-mybuffer[] = rank
+ans = myAllReduce(rank, size, rank, nb)
 
-left = rank - 1
-right = rank + 1
-if ( right > size-1) {
-    right  = 0
-}
-if ( left < 0 ) {
-    left = size-1
-}
-
-print(sprintf("My rank: %d, left: %d, right %d;", rank, left, right))
-
-## Communication
-parity = rank %% 2
-
-offset = 1
-for (i in 1:size-1) {
-    mtag = 1
-    if (parity == 0) {
-        svals = mybuffer[offset:offset+nb-1]
-        send(svals, rank.dest=right, tag=mtag)
-
-        offset = offset+nb
-        rvals = mybuffer[offset:offset+nb-1]
-        recv(rvals, rank.source=left, tag=mtag)
-        mybuffer[offset:offset+nb-1] = rvals
-    }
-    else {
-        offset = offset+nb
-        rvals = mybuffer[offset:offset+nb-1]
-        recv(rvals, rank.source=left, tag=mtag)
-        mybuffer[offset:offset+nb-1] = rvals
-
-        svals = mybuffer[offset-nb:offset-1]
-        send(svals, rank.dest=right, tag=mtag)
-    }
-}
-
-cat('Buffer sum is: ')
-cat(sum(mybuffer))
-
+print(ans)
+    
 finalize()
 
