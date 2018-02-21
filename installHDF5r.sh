498c626692bf54fee9a64737d44eba499f4f2472
Thomas Hauser <thauser@igskahcmgslih04.cr.usgs.gov>
2018-02-21 11:27:38 -0700

    removed comment

--- installHDF5r.sh
+++ installHDF5r.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wget https://cran.r-project.org/src/contrib/hdf5r_1.0.0.tar.gz
+wget https://cran.r-project.org/src/contrib/hdf5r_1.0.0.tar.gz
 
 module purge
 module load gcc/6.1  hdf5-serial/1.8.18-gcc6.1.0 zlib/1.2.11-gcc
