f370a4f5500b4445f0d90b6c470aab7301d4b968
Thomas Hauser <thauser@igskahcmgslih04.cr.usgs.gov>
2018-02-21 11:10:58 -0700

    serial install of HDF5r

--- installHDF5r.sh
+++ installHDF5r.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-wget https://cran.r-project.org/src/contrib/hdf5r_1.0.0.tar.gz
+#wget https://cran.r-project.org/src/contrib/hdf5r_1.0.0.tar.gz
 
 module purge
-module load gcc/6.1 openmpi/1.10.2-gcc6.1.0 hdf5-parallel/1.8.18-gcc6.1.0 zlib/1.2.11-gcc
+module load gcc/6.1  hdf5-serial/1.8.18-gcc6.1.0 zlib/1.2.11-gcc
 
-export HDF5_ROOT="/cxfs/projects/spack/opt/spack/linux-scientific6-x86_64/gcc-6.1.0/hdf5-1.8.18-5d7vrctlkalwf2bga4xrltdokbxvntb4"
-
-R CMD INSTALL hdf5r_1.0.0.tar.gz --configure-args="--with-hdf5=$HDF5_ROOT/bin/h5pcc"
+R CMD INSTALL hdf5r_1.0.0.tar.gz
